880fb3415dff0ea59e998550df8744802cd116a7
Jan Noel S. Paed <Jannoelpaed17@gmail.com>
2025-03-06 01:48:32 +0800

    added a routes and navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
 import React from "react";
-
+import { Routes, Route } from "react-router-dom";
+import Favorites from "./pages/favorites";
+import Home from "./pages/home";
+import Details from "./pages/details";
+import Navbar from "./components/navbar";
 const App = () => {
-  return <div className="text-2xl bg-red-200 font-bold">App</div>;
+  return (
+    <div className="bg-red-200">
+      <div className="min-h-screen p-6 bg-white text-gray-600 text-lg">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/Favorites" element={<Favorites />} />
+          <Route path="/recipe-item/:id" element={<Details />} />
+        </Routes>
+      </div>
+    </div>
+  );
 };
 
 export default App;
